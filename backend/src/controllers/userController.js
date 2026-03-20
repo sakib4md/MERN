6c9f16da9d8772d9858c876b54249a41d87e935e
sakibmd1188 <sakibmdcloud1999@gmail.com>
2026-03-20 09:53:30 +0530

    Added react query

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -28,6 +28,7 @@ const registerUser = async (req, res, next) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        createdAt: user.createdAt,
       },
     });
   } catch (err) {
